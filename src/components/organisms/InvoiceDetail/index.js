1d481c7fec6a21ae088fb9110fb077ff07a0e5ea
Noober1 <cucu.ruhiyatna3@gmail.com>
2021-11-21 19:50:54 +0700

    working on InvoiceDetail component

--- src/components/organisms/InvoiceDetail/index.js
+++ src/components/organisms/InvoiceDetail/index.js
@@ -1,15 +1,16 @@
 import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Skeleton, Typography } from '@mui/material'
+import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Paper, Skeleton, Typography } from '@mui/material'
 import useLocalization from '../../../lib/useLocalization'
 import { useFetchApi } from '../../../lib'
 import { selectAuth } from '../../../lib/redux/slices/authSlice'
 import { useSelector } from 'react-redux'
 import { useUpdateEffect } from 'react-use'
 import fetchAPI, { fetchWithToken } from '../../../lib/fetchApi'
-import { DetailPaper } from '..'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import { tools } from '../../../lib'
 import DetailText from './DetailText'
+import { DataGrid } from '@mui/x-data-grid'
 
 const InvoiceDetail = forwardRef((props,ref) => {
     const { default:defaultText, components: {invoiceDetailDialog} } = useLocalization()
@@ -30,7 +31,20 @@ const InvoiceDetail = forwardRef((props,ref) => {
     const url = `/administrasi/getInvoice/?code=${invoiceCode}`
 
     const openDialog = () => setopen(true)
-    const closeDialog = () => setopen(false)
+    const closeDialog = () => {
+        if (fetchError) {
+            setfetchData({})
+            setfetchLoading(true)
+            setfetchError(false)
+            setfetchErrorMessage('')
+            setinvoiceCode(null)
+        }
+        setopen(false)
+    }
+
+    const helpButtonHandler = () => {
+
+    }
 
     useUpdateEffect(() => {
         if (invoiceCode && open) {
@@ -48,13 +62,11 @@ const InvoiceDetail = forwardRef((props,ref) => {
                 setfetchLoading(false)
                 setfetchData(result)
                 setdetailList([
-                    {title: invoiceDetailDialog.invoiceDate, content: tools.dateFormatting(result.date.invoice || new Date(), 'd M y', defaultText.nameOfMonths)},
+                    {title: invoiceDetailDialog.paymentMethod, content: result.paymentMethod},
                     {title: invoiceDetailDialog.refNumber, content: result.refNumber || '-'},
-                    {title: invoiceDetailDialog.sentTo, content: result.student.fullName},
-                    {title: invoiceDetailDialog.status, content: result.status},
                     {title: invoiceDetailDialog.accountNumber, content: result.accountNumber || '-'},
-                    {title: invoiceDetailDialog.destinationAccount, content: result.destinationAccount || '-'},
                     {title: invoiceDetailDialog.transactionDate, content: result.date.transaction || '-'},
+                    {title: invoiceDetailDialog.destinationAccount, content: result.destinationAccount || '-'},
                     {title: invoiceDetailDialog.verificationDate, content: result.date.verification || '-'}
                 ])
             })
@@ -115,7 +127,7 @@ const InvoiceDetail = forwardRef((props,ref) => {
                                         </div>
                                     </div> :
                                     <div>
-                                        <Typography variant="h4" align="center" className="uppercase font-bold" color={fetchData.status == 'unpaid' ? 'red' : 'green'}>
+                                        <Typography variant="h5" align="center" className="uppercase font-bold" color={fetchData.status == 'unpaid' ? 'red' : 'green'}>
                                             {fetchData.status == 'unpaid' ? invoiceDetailDialog.statusUnpaid :
                                             fetchData.status == 'paid' ? invoiceDetailDialog.statusPaid :
                                             fetchData.status == 'confirming' ? invoiceDetailDialog.statusConfirming :
@@ -130,6 +142,87 @@ const InvoiceDetail = forwardRef((props,ref) => {
                                 }
                             </div>
                         </div>
+                        <div className='col-span-3 grid grid-cols-2 gap-4 mb-5'>
+                            <div>
+                                <Typography variant="h6">
+                                    {loading ? <Skeleton width="50%"/> : invoiceDetailDialog.sentTo}
+                                </Typography>
+                                <Typography className="capitalize">
+                                    {loading ? <Skeleton/> : fetchData?.student?.fullName
+                                    }
+                                </Typography>
+                            </div>
+                            <div className="text-right">
+                                <Typography variant="h6">
+                                    {loading ? <div className="grid grid-cols-4">
+                                        <span></span>
+                                        <Skeleton className="col-span-3"/>
+                                    </div> : invoiceDetailDialog.destinationAccount}
+                                </Typography>
+                                {loading ?
+                                    <div className="grid grid-cols-2">
+                                        <span></span>
+                                        <Skeleton/>
+                                        <span> </span>
+                                        <Skeleton/>
+                                    </div> : 
+                                    <Typography>
+                                        BANK CENTRAL ASIA(BCA)<br/>
+                                        1209389238901283
+                                    </Typography>
+                                }
+                            </div>
+                        </div>
+                        <div className="col-span-3">
+                            {loading ? 
+                                <div className="block">
+                                    <Skeleton height="150px"/>
+                                </div> :
+                                <DataGrid
+                                    disableColumnFilter
+                                    disableColumnMenu
+                                    disableDensitySelector
+                                    autoHeight
+                                    columns={[
+                                        {
+                                            field: 'date',
+                                            headerName: invoiceDetailDialog.invoiceDate,
+                                            width:200,
+                                            valueGetter: params => tools.dateFormatting(params.value || new Date(), 'd M y', defaultText.nameOfMonths)
+                                        },
+                                        {
+                                            field:'description',
+                                            headerName: invoiceDetailDialog.paymentType,
+                                            flex:1,
+                                            renderCell: params => (
+                                                <>
+                                                    <Typography variant="body1">
+                                                        {params.value}<br/>
+                                                        <Typography variant="caption" noWrap>
+                                                            {fetchData?.payment?.description || ''}
+                                                        </Typography>
+                                                    </Typography>
+                                                </>
+                                            )
+                                        },
+                                        {
+                                            field:'price',
+                                            headerName: invoiceDetailDialog.paymentPrice,
+                                            width:250
+                                        },
+                                    ]}
+                                    rows={[
+                                        {
+                                            id: 1,
+                                            date: fetchData?.date?.invoice,
+                                            description: fetchData?.payment?.type || '',
+                                            price: tools.rupiahFormatting(fetchData?.payment?.price || 0)
+                                        }
+                                    ]}
+                                    hideFooter
+                                />
+                            }
+                        </div>
                         <div className="col-span-3">
                             <DetailText
                                 loading={loading}
@@ -138,6 +231,9 @@ const InvoiceDetail = forwardRef((props,ref) => {
                         </div>
                     </DialogContent>
                     <DialogActions className="p-5">
+                        <Button variant="contained" size="large" color="secondary" onClick={helpButtonHandler} startIcon={<HelpOutlineIcon/>}>
+                            {defaultText.helpButtonLabel}
+                        </Button>
                         <Button variant="contained" size="large">
                             {invoiceDetailDialog.actionSendPaymentDetail}
                         </Button>
